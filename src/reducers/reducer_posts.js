50db7f70915209744e06078f94d24a1ca790095f
g01198628y <g01198628y@gmail.com>
2017-05-04 20:48:22 +0800

    show the post and cache records

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,17 @@
 import _ from 'lodash';
-import {FETCH_POSTS} from '../actions';
+import {FETCH_POSTS,FETCH_POST} from '../actions';
 
 export default function (state= {} ,action){
   switch (action.type){
+  case FETCH_POST:
+    //const post = action.paylod.data;
+    //const newState = { ...state };
+    //newState[post.id] = post ;
+    //return newState; 這4行=下面那一行
+
+    return {...state,[action.payload.data.id]:action.payload.data};
+
+
   case FETCH_POSTS:
     return _.mapKeys(action.payload.data,'id');
     default:
