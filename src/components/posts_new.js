e36385cf183736775ebb0c52a0fd2ca43d65f0bb
g01198628y <g01198628y@gmail.com>
2017-05-04 15:22:59 +0800

    validate and use css show error

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,30 +3,42 @@ import { Field,reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
   renderField(field){
+    const { meta : {touched,error} } = field ;
+    const className=`form-group ${touched && error ? 'has-danger' : ''}`;
     return(
-      <div className="form-group">
+      <div className={className}>
       <label>{field.label}</label>
       <input
       className="form-control"
       type="text"
         {...field.input}
       />
+      <div className="text-help">
+      {touched ? error: ''}
+      </div>
       </div>
     );
   }
 
 
+onSubmit(values){
+  //this === component
+  console.log(values);
+}
+
   render(){
+    const{handleSubmit}=this.props;
+
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
         label="Title For Post"
         name="title"
         component={this.renderField}
         />
         <Field
-        label="Tags"
-        name="tags"
+        label="Categories"
+        name="categories"
         component={this.renderField}
         />
         <Field
@@ -34,11 +46,35 @@ class PostsNew extends Component{
         name="content"
         component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
 }
 
+
+function validate(values){
+  //console.log(values)->{titlte:'asdf',categories:'asdf',content:'asdf'}
+  const errors={};
+  //validate the unputs from 'values'
+
+  if(!values.title){
+    errors.title="Enter a title";
+  }
+  if(!values.categories){
+    errors.categories ="Enter some categories";
+  }
+  if(!values.content){
+    errors.content="Enter some content please";
+  }
+
+  //if errors is empty ,the form is fine to submit
+  //if errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
+
 export default reduxForm({
+  validate, //==validate:validate
   form:'PostsNewForm' //or form:'1234567980' unique就可
 })(PostsNew);
